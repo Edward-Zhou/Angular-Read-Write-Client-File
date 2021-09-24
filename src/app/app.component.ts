1a585d9261637d1dbee5fad25cb6d38c2d2e64b2
Edward-Zhou <v-tazho@hotmail.com>
2021-09-24 15:43:58 +0800

    angular wait for observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { saveAs } from 'file-saver';
+import { Observable, of } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { Student } from './Student';
 
 @Component({
   selector: 'my-app',
@@ -7,45 +11,102 @@ import { saveAs } from 'file-saver';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  @ViewChild('fileUpload', { static: false }) fileUpload: ElementRef;
-  saveFileWithLink() {
-    var data = {
-      test: 'Hello World',
-    };
-    var json = JSON.stringify(data);
-    var blob = new Blob([json], { type: 'application/json' });
-    var url = window.URL.createObjectURL(blob);
-    var a = document.createElement('a');
-    a.href = url;
-    a.download = 'test.json';
-    a.click();
-  }
-  saveFileWithFileSaver() {
-    var data = {
-      test: 'Hello World',
-    };
-    var json = JSON.stringify(data);
-    var file = new File([json], 'test.json', {
-      type: 'application/json;charset=utf-8',
+  constructor(private client: HttpClient) {}
+  async async_await_SolutionTest() {
+    var result = await this.async_await_Solution();
+    result.forEach((student) => {
+      console.log(student.score);
     });
-    saveAs(file);
   }
-  upload(event): void {
-    console.log(event.target.files[0].name);
+  async async_await_Solution(): Promise<Student[]> {
+    var students = await this.getStudents().toPromise();
+    students.forEach(async (student) => {
+      if (student.score == 60) {
+        var res = this.client.get(
+          'https://api.coindesk.com/v1/bpi/currentprice.json'
+        );
+        console.log(res);
+        student.score = student.score * 100;
+      } else {
+        student.score = student.score * 10;
+      }
+    });
+    return students;
   }
-  uploadDynamic() {
-    const fileUpload = this.fileUpload.nativeElement;
-    fileUpload.onchange = () => {
-      const file = fileUpload.files[0];
-      console.log(file.name);
-    };
-    fileUpload.click();
+  createObvervableSolutionTest() {
+    this.createObvervableSolution()
+      .pipe(
+        mergeMap((result) => {
+          if (result && result.length > 0) {
+            result.forEach((s) => console.log(s.score));
+          }
+          return of([]);
+        })
+      )
+      .subscribe();
   }
-  readFile(file: File) {
-    var reader = new FileReader();
-    reader.onload = () => {
-      console.log(reader.result);
-    };
-    reader.readAsText(file);
+  createObvervableSolution(): Observable<Student[]> {
+    return this.getStudents().pipe(
+      mergeMap((result) => {
+        return new Observable<Student[]>((subscriber) => {
+          if (result) {
+            result.map((rr) => {
+              if (rr.score == 60) {
+                this.client
+                  .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                  .subscribe((res) => {
+                    console.log(res);
+                    rr.score = rr.score * 100;
+                    subscriber.next([rr]);
+                  });
+              } else {
+                rr.score = rr.score * 10;
+                subscriber.next([rr]);
+              }
+            });
+          }
+        });
+      })
+    );
+  }
+  reproduce(): Observable<Student[]> {
+    return this.getStudents().pipe(
+      mergeMap((result) => {
+        if (result) {
+          result.map((rr) => {
+            if (rr.score == 60) {
+              this.client
+                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                .subscribe((res) => {
+                  console.log(res);
+                  rr.score = rr.score + 100;
+                });
+            } else {
+              rr.score = rr.score * 10;
+            }
+            return rr;
+          });
+        }
+        return of(result);
+      })
+    );
+  }
+  reproduceTest() {
+    this.getStudents()
+      .pipe(
+        mergeMap((result) => {
+          if (result && result.length > 0) {
+            result.forEach((s) => console.log(s.score));
+          }
+          return of([]);
+        })
+      )
+      .subscribe();
+  }
+  getStudents(): Observable<Student[]> {
+    return of(this.students());
+  }
+  students(): Student[] {
+    return [{ score: 50 }, { score: 60 }, { score: 70 }];
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:  [ BrowserAnimationsModule, ReactiveFormsModule, BrowserModule, FormsModule, MaterialModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    HttpClientModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
